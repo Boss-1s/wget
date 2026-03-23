a578da5cf932f5c9d3c213597fe5fc37061e9ee4
Boss_1s <95505913+Boss-1s@users.noreply.github.com>
2026-03-23 16:56:17 -0500

    Update wav_read.py

--- wav_read.py
+++ wav_read.py
@@ -1,4 +1,4 @@
-import wave,os
+import wave,os,librosa,soundfile as sf
 
 wf = os.environ.get('LINK')
 
@@ -16,3 +16,13 @@ with wave.open(wf, 'rb') as f:
 print(f"Sample Rate: {sample_rate} Hz")
 print(f"Bit Depth: {bit_depth}-bit")
 print(f"Channels: {channels}")
+
+if os.environ.get('RE'):
+    # Load at original sample rate
+    y, sr = librosa.load(wf, sr=None) 
+    
+    # Resample to 16kHz
+    y_16k = librosa.resample(y, orig_sr=sr, target_sr=11025)
+    
+    # Save the result
+    sf.write(wf, y_16k, 11025)
